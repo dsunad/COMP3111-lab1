df2ee67cc4a79624eff834ca394cb96beb8fb038
Misika Mei <Misika Mei@LAPTOP-8PCFDK57>
2021-02-17 21:34:35 +0800

    made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -8,6 +8,9 @@ public class Library {
         return true;
     }
     public static void main(String arg[]) {
+    	Library lib = new Library();
+    	if(lib.someLibraryMethod()) {
     	System.out.println("Hello, World");
+    	}
     }
 }
